30fbf3914121f8e3b945e0c9fa73b05fc23d1977
Yassine-Zaazaa <yassine.zaazaa@um6p.ma>
2021-06-25 17:03:25 +0100

    EX4

--- EX4/brackets.c
+++ EX4/brackets.c
@@ -1,52 +1,53 @@
 #include "stack.h"
 
-int     recursive_check(/* add parameter */)
+const char *recursive_check(const char *C)
 {
-        // write recursive method for checking
+        if( *C == '\0' || *C == ')' || *C == ']' || *C == '}') 
+                return C; 
+                
+        if( *C == '(' )
+        {
+                const char *C2 = recursive_check(++C);
+                if( *C2 == ')' || *C2 == ']' || *C2 == '}')
+                {
+                        return recursive_check(++C2);
+                }
+                return C - 1;
+        }
+
+        return recursive_check(++C);
 }
 
-int pairs(char  C1, char C2){
+
+int     pairs(char C1, char C2)
+{
     if (C1 == '(' && C2 == ')')
-        return 1;
-    if (C1 == '[' && C2 == ']')
         return 1;
     if (C1 == '{' && C2 == '}')
         return 1;
-    return 0;
+    if (C1 == '[' && C2 == ']')
+        return 1;
+    else
+        return 0;
 }
 
-int     iterative_check(char * C)
+int     iterative_check(char C[])
 {
-    t_stack * P = NULL;
-    for(int i = 0; i < strlen(C); i++)
+    int i = 0;
+    t_stack *P = NULL;
+    while (C[i])
     {
-        printf("%c\n", C[i]);
-        if(C[i] == '(' || C[i] == '[' || C[i] == '{')
-                push(&P, C[i]);
-        
-        if(C[i] == ')')
-        {
-            if(P == NULL)
+        if (C[i] == '(' || C[i] == '[' || C[i] == '{')
+            push(&P, C[i]);
+        if (C[i] == ')' || C[i] == ']' || C[i] == '}') {
+            if (P == NULL)
                 return 0;
-            if(pairs(C[i], pop(&P)) == 0)
+            if (pairs(pop(&P), C[i]) == 0)
                 return 0;
         }
-        if(C[i] == ']')
-        {
-            if(P == NULL)
-                return 0;
-            if(pairs(C[i], pop(&P)) == 0)
-                return 0;
-        }
-        if(C[i] == '}')
-        {
-            if(P == NULL)
-                return 0;
-            if(pairs(C[i], pop(&P)) == 0)
-                return 0;
-        }
+        i++;
     }
-    if(P == NULL)
+    if (P == NULL)
         return 1;
     else
         return 0;
@@ -59,11 +60,14 @@ int main(int ac, char ** av)
     }
 
     int res = iterative_check(av[1]);
-    int res2 = recursive_check(av[1]);
     printf("We begin with the iterative check\n");
-    if(res == 0)
+    if(res == 1)
         printf("The string is well formatted\n");
-    else if(res == -1)
+    else if(res == 0)
         printf("The string isn't formatted\n");
+    printf("Now the recursive check");
+    const char * C2 = recursive_check(av[1]);
+    printf("%s\n", C2);
+    return 0;
 }
 
